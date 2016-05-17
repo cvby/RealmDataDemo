60014ff7b0d3c8a349521bcf3926e66c3257738c
李政 <lizhengchaoqiang@163.com>
2016-05-17 17:26:47 +0800

    增加添加数据方法

--- RalemData/RelamData/RelamData/ViewController.m
+++ RalemData/RelamData/RelamData/ViewController.m
@@ -17,12 +17,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self createData];
+    //[self createData];
+    [self createDataWithBlock];
     [self loadData];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
--(void)createData{
+-(ProvinceEntity*)createModel:(NSArray*)array{
     ProvinceEntity* province=[[ProvinceEntity alloc] init];
     province.sId =1;
     province.pid = 2;
@@ -30,9 +31,8 @@
     province.shortName = @"江苏";
     province.enName = @"jiangsu";
     province.prefixLetter = @"j";
-    
-    NSArray* array=@[@"无锡",@"苏州",@"常州",@"南京",@"镇江"];
-    for (int i=0; i<5; i++) {
+    NSArray* aArray=@[@"滨湖区",@"锡山区",@"南长区",@"崇安区",@"北塘区"];
+    for (int i=0; i<array.count; i++) {
         CityEntity *city = [[CityEntity alloc] init];
         city.sId =1;
         city.pid = 3;
@@ -40,9 +40,24 @@
         city.shortName = array[i];
         city.enName = @"wuxi";
         city.prefixLetter = @"w";
+        for (int j=0; j<5; j++) {
+            AreaEntity *area = [[AreaEntity alloc] init];
+            area.sId =1;
+            area.pid = 3;
+            area.type = 0;
+            area.shortName = aArray[j];
+            area.enName = @"wuxi";
+            area.prefixLetter = @"w";
+            [city.areas addObject:area];
+        }
         [province.citys addObject:city];
     }
+    return province;
+}
+
+-(void)createData{
     
+    ProvinceEntity* province=[self createModel:@[@"无锡",@"苏州",@"常州",@"南京",@"镇江"]];
     // Get the default Realm
     RLMRealm *realm = [RLMRealm defaultRealm];
     // You only need to do this once (per thread)
@@ -51,6 +66,13 @@
     [realm beginWriteTransaction];
     [realm addObject:province];
     [realm commitWriteTransaction];
+}
+
+-(void)createDataWithBlock{
+    ProvinceEntity* province=[self createModel:@[@"徐州",@"淮安",@"南京",@"扬州",@"盐城",@"南通",@"连云港"]];
+    [[RLMRealm defaultRealm] transactionWithBlock:^{
+        [[RLMRealm defaultRealm] addObject:province];
+    }];
 }
 
 -(void)loadData{
